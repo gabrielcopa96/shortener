d975b43fccb1d31a865f192308517d7ffd0aaff4
Gabriel <gabrielcncopa@gmail.com>
2022-10-07 22:01:51 -0300

    fix: toast

--- app-acortador/src/main.jsx
+++ app-acortador/src/main.jsx
@@ -1,14 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
 import axios from 'axios'
 
-// const URL = process.env.API_URL || 'https://short-0j0q.onrender.com';
+import App from './App'
+import './index.css'
 
 axios.defaults.baseURL = 'https://short-0j0q.onrender.com' || 'http://localhost:3001';
 
-//  
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
